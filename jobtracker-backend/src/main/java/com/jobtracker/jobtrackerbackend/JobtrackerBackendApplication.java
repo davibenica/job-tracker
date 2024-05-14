04d4df51a626b141704fa6766f6c2aa48b12183e
davibenica <davibenica@Davis-MacBook-Air.local>
2024-05-14 14:26:40 -0400

    fixed dir issues

---
+++ jobtracker-backend/src/main/java/com/jobtracker/jobtrackerbackend/JobtrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jobtracker.jobtrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobtrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobtrackerBackendApplication.class, args);
+	}
+
+}
